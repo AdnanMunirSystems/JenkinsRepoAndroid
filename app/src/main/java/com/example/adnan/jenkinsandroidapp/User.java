b129f1931b8fea0c3b68b9606f29945f1384969d
AdnanMunirSystems <adnan.munir@systmesltd.com>
2016-07-28 14:47:41 +0500

    Updates

---
+++ app/src/main/java/com/example/adnan/jenkinsandroidapp/User.java
@@ -0,0 +1,16 @@
+package com.example.adnan.jenkinsandroidapp;
+
+/**
+ * Created by Adnan on 7/28/16.
+ */
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
